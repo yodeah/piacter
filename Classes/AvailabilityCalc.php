4876458c1e3596aa3fba357edb8184bf5a3217f2
neandrw <neandrw@gmail.com>
2014-04-27 23:37:36 +0200

    doksi update

--- Classes/AvailabilityCalc.php
+++ Classes/AvailabilityCalc.php
@@ -2,7 +2,7 @@
 
 class AvailabilityCalc {
 
-    static function Calc() {
+    public static function Calc() {
         $type = Values::$AVAILBILITY_EN[$_POST['availability_ty']];
         $quantity = $_POST['availability'];
         return date("Y-m-d H:i:s", strtotime('+'.$quantity.' '.$type ));
